471bc499599290b3c3fae04b176c8cd15e8c9192
Alex Lamb <alexlamb104@gmail.com>
2022-10-10 00:17:27 -0600

    First styling for form

--- src/components/TrackerForm.js
+++ src/components/TrackerForm.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./ShotTracker.css";
 
 function TrackerForm() {
   return (
-    <div>
-      <form>
+    <div className="form-div">
+      <form className="each-form">
         <input type="number" min={0} />
         <input type="number" min={0} />
         <p>Total:</p>
