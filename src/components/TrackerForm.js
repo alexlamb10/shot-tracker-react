257ab533f1f4d6443f4d438ceed3904e1a58f068
Alex Lamb <alexlamb104@gmail.com>
2022-10-10 22:02:42 -0600

    Added dropdown

--- src/components/TrackerForm.js
+++ src/components/TrackerForm.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ShotTracker.css";
 
 function TrackerForm() {
+  const [currentPlayer, setCurrentPlayer] = useState("");
+  let names = [
+    { playerId: 1, player: "Alex" },
+    { playerId: 2, player: "Jaren" },
+    { playerId: 3, player: "Blake" },
+    { playerId: 4, player: "Preston" },
+    { playerId: 5, player: "Zack" },
+  ];
+
+  let nameList = names.map((name) => (
+    <option value={name.player}>{name.player}</option>
+  ));
+
   return (
     <div className="form-div">
+      <h1>{currentPlayer ? `${currentPlayer}'s Score` : "Select a Player"}</h1>
+      <select onChange={(e) => setCurrentPlayer(e.target.value)}>
+        <option value="">Select Player</option>
+        {names.map((name) => (
+          <div>{name.player}</div>
+        ))}
+        {nameList}
+      </select>
       <form className="each-form">
         Free Throws:
         <div className="drill-inputs">
